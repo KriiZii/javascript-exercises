568a4eb42f60a966a90930005e800826e536cb45
KriiZii <39064251+KriiZii@users.noreply.github.com>
2024-08-05 05:36:43 +0200

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+          }
+    });
+    return newArray;
 };
 
-// Do not edit below this line
-module.exports = removeFromArray;
+console.log(removeFromArray([1,2,3,4,5,6,7,8],3));
