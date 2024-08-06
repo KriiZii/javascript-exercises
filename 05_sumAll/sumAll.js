9b56db719d881fb5c04b593f40843464eb1d3f53
KriiZii <39064251+KriiZii@users.noreply.github.com>
2024-08-07 00:06:18 +0200

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let number = 0;
+    if(a>b) {
+        for(let i = b; i <= a; i++) {
+            number += i;
+        }
+        return number;
+    }
+    else if(a<b) {
+        for(let i = a; i <= b; i++) {
+            number += i;
+        }
+        return number;
+    }
+    else {
+        for(let i = a; i <= b; i++) {
+            number += i;
+        }
+        return number;
+    }
 };
-
-// Do not edit below this line
-module.exports = sumAll;
