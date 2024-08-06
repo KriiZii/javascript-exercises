4192d3ff8061c4dfee1a11fa0f054e23e3609358
KriiZii <39064251+KriiZii@users.noreply.github.com>
2024-08-07 00:11:26 +0200

    completed 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year%4===0) {
+        if(year%100===0) {
+            if(year%400===0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
 };
-
-// Do not edit below this line
-module.exports = leapYears;
